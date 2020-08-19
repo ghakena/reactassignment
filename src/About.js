5fcc0d1f4783a0a21e4c3518a62f5bcd55bf92b2
rokuirie <akenagibbsherbert6@gmail.com>
2020-08-19 23:32:55 +0300

    modified:   src/About.js 	modified:   src/Home.js 	modified:   src/Navbar.js 	new file:   src/images/mic.jpg 	new file:   src/images/voice.jpg 	modified:   src/style.css 	added a couple images to the images dir within src folder

--- src/About.js
+++ src/About.js
@@ -3,12 +3,17 @@ import React from 'react';
 function About() {
     return (
         <div className="about-div">
+            <div className="bgn-img"></div>
             <div class="jumbotron">
                 <h1 class="display-4">ABOUT US</h1>
                 <p class="lead">
                     We are a multinational company that is heavily invested in the entertainment industry and we offer a couple of services including but not limited to 
                     renting out instruments, stage preparation for concerts, instrument sales, instrument maintenance, music production, among others.
                 </p>
+                <div class="lead">
+                    We seek to promote talent, a productive work ethic for artistes, quality in the producion of music, shows and many other platforms that actively involve
+                    the musicians.
+                </div>
 
                 <hr class="my-4" />
                 <p class="contact-text">Get in contact with us for any of our services above using these link below</p>
